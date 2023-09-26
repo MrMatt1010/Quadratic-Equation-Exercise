85b052c68f53835e261b9b371628a5cd55d7e9cc
Matt T <mrsymtec@gmail.com>
2023-09-26 17:10:54 +1300

    Added comments to code, tested okay

--- Quadratic Equation/Quadratic Equation.cpp
+++ Quadratic Equation/Quadratic Equation.cpp
@@ -15,6 +15,7 @@ int main()
 	std::cout << "Enter coefficient c: ";
 	std::cin >> c;
 
+	//Calculations are completed here and output
 	double discriminant = b * b - 4 * a * c;
 
 	if (discriminant < 0) {
@@ -28,9 +29,9 @@ int main()
 	else {
 		double x1 = (-b + sqrt(discriminant)) / (2 * a);
 		double x2 = (-b - sqrt(discriminant)) / (2 * a);
-		std::cout << "The roots are real and unequal" std::endl;
+		std::cout << "The roots are real and unequal" << std::endl;
 		std::cout << "x1 = " << x1 << std::endl;
-		std::cout << "x2 = " << x2 << std::cout;
+		std::cout << "x2 = " << x2 << std::endl;
 	}
 	return(0);
 }
