e6b70d216e4a3fe52bd34cc0e45a86283b574b79
Matt T <mrsymtec@gmail.com>
2023-09-26 16:59:37 +1300

    Added inputs for the coefficicents and started on the discriminant portion

--- Quadratic Equation/Quadratic Equation.cpp
+++ Quadratic Equation/Quadratic Equation.cpp
@@ -5,5 +5,17 @@
 
 int main()
 {
-   
+	double a, b, c;
+
+	//This is the input for the coefficients a, b, and c
+	std::cout << "Enter coefficient a: ";
+	std::cin >> a;
+	std::cout << "Enter coefficient b: ";
+	std::cin >> b;
+	std::cout << "Enter coefficient c: ";
+	std::cin >> c;
+
+	double discriminant = b * b - 4 * a * c;
+
+
 }
