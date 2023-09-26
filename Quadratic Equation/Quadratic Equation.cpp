90b2ba0177a3d79052b18747f342eb58613b50b5
Matt T <mrsymtec@gmail.com>
2023-09-27 09:44:19 +1300

    Made changes to the code layout to get it to align better with YooBees desired output

--- Quadratic Equation/Quadratic Equation.cpp
+++ Quadratic Equation/Quadratic Equation.cpp
@@ -14,8 +14,8 @@ int main()
 	std::cin >> b;
 	std::cout << "Enter coefficient c: ";
 	std::cin >> c;
-
-	//Calculations are completed here and output
+	std::cout << "\n";
+	//Calculations are completed here and output is displayed 
 	double discriminant = b * b - 4 * a * c;
 
 	if (discriminant < 0) {
@@ -29,9 +29,8 @@ int main()
 	else {
 		double x1 = (-b + sqrt(discriminant)) / (2 * a);
 		double x2 = (-b - sqrt(discriminant)) / (2 * a);
-		std::cout << "The roots are real and unequal" << std::endl;
-		std::cout << "x1 = " << x1 << std::endl;
-		std::cout << "x2 = " << x2 << std::endl;
+		std::cout << "The roots are real and unequal\n" << std::endl;
+		std::cout << "x1 = " << x1 << "  x2 = " << x2 << std::endl;
 	}
 	return(0);
 }
