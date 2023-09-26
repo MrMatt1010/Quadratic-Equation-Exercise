188d03cde8fcdb3cc2a9ae1fb6544d4c7b5301b4
Matt T <mrsymtec@gmail.com>
2023-09-26 17:07:36 +1300

    Completed the discriminant portion of code and the outputs.

--- Quadratic Equation/Quadratic Equation.cpp
+++ Quadratic Equation/Quadratic Equation.cpp
@@ -17,5 +17,20 @@ int main()
 
 	double discriminant = b * b - 4 * a * c;
 
-
+	if (discriminant < 0) {
+		std::cout << "The roots are imaginary" << std::endl;
+	}
+	else if (discriminant == 0) {
+		double x = -b / (2 * a);
+		std::cout << "The roots are real and equal" << std::endl;
+		std::cout << "x1 = x2 " << x << std::endl;
+	}
+	else {
+		double x1 = (-b + sqrt(discriminant)) / (2 * a);
+		double x2 = (-b - sqrt(discriminant)) / (2 * a);
+		std::cout << "The roots are real and unequal" std::endl;
+		std::cout << "x1 = " << x1 << std::endl;
+		std::cout << "x2 = " << x2 << std::cout;
+	}
+	return(0);
 }
